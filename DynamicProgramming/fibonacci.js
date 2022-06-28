eb3b8e0691668a9e32794ec9ad2005ab6619c1f5
Benjamin Cai (蔡子豪) <41190525+code0312@users.noreply.github.com>
2022-06-28 16:38:34 +0000

    fib fn

--- DynamicProgramming/fibonacci.js
+++ DynamicProgramming/fibonacci.js
@@ -1,3 +1,8 @@
-function fibonacci(n) {
-  
+function fibonacci(n, memo={}) {
+  if(n in memo) return memo[n]
+  if (n <= 1) return n
+  memo[n] = fibonacci(n-1) + fibonacci(n-2)
+  return memo[n]
 }
+
+console.log(fibonacci(10))
